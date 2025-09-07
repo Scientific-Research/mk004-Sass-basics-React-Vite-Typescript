6b4bba36cd6257665ba401dfd267faeadd53137d
Maximilian <scientificresearch309@gmail.com>
2025-09-07 21:03:35 +0200

    defining some scss variables in css

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Sass-basics-React-Vite-Typescript</h1>
+      <p>Welcome to this Site!</p>
+      <h2>Detailed Info</h2>
+      <p>sdlkjflksdjfkljsdfkljsdlkfjslkdf</p>
+      <h3>More Info</h3>
+      <p>lskdjflksdjflksHJDFKLHSDFL</p>
     </div>
   );
 }
